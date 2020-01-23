d403e38f5e979bcb288cdf8d2a409f95489f3c7a
jg116907 <jg116907@naver.com>
2020-01-23 17:26:59 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Counter from './Counter';
+import Info from './Info';
+
+const App = () => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => {
+          setVisible(!visible)
+        }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+      {
+      visible && 
+        <div>
+          <Info />
+          <Counter />
+        </div>
+      }
+    </div>
+  );
+};
+
+export default App;
