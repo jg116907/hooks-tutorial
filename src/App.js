db53e99f4641d995db8c9afa8e9ee5e6dbd5535e
jg116907 <jg116907@naver.com>
2020-01-25 02:23:02 +0900

    ADD useMemo and useCallback

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-import React, { useState } from 'react';
-import Counter from './Counter';
-import Info from './Info';
+import React from 'react';
+import Average from './Average';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-
-  return (
-    <div>
-      <button onClick={() => {
-          setVisible(!visible)
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {
-      visible && 
-        <div>
-          <Info />
-          <Counter />
-        </div>
-      }
-    </div>
-  );
+  return <Average />;
 };
 
 export default App;
