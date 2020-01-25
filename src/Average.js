817cef6521ca71c21f4111e9861c7e012c0e2e30
jg116907 <jg116907@naver.com>
2020-01-25 13:33:16 +0900

    ADD useRef and useInputs

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, { useState, useMemo, useCallback, useRef } from 'react';
 
 const getAverage = numbers => {
     console.log("계산중..");
@@ -10,34 +10,25 @@ const getAverage = numbers => {
 const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
+    const inputEl = useRef(null); // ref 안의 값은 바뀌어도 컴포넌트 렌더링이 되지 않는다.
 
-    // const onChange = e => {
-    //     setNumber(e.target.value)
-    // };
     const onChange = useCallback(e => {
         setNumber(e.target.value)
     }, []); // 컴포넌트가 처음 렌더링 될 때만 함수 생성
 
-    // const onInsert = e => {
-    //     const nextList = list.concat(parseInt(number));
-    //     setList(nextList);
-    //     setNumber('');
-    // };
     const onInsert = useCallback(e => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
+        inputEl.current.focus(); // 등록 버튼을 누르면 포커스가 input쪽으로 이동
     }, [number, list]); // numbers, list 가 바뀌었을 때만 함수 생성
-    // useCallback(function, change_list)
-    // 일반 값 재사용 => useMemo
-    // 함수 재사용 => useCallback
 
     // 렌더링 과정에서 특정 값이 바뀌었을 때만 연산을 실행
     const avg = useMemo(() => getAverage(list), [list]); 
 
     return (
         <div>
-            <input value={number} onChange={onChange} />
+            <input value={number} onChange={onChange} ref={inputEl}/>
             <button onClick={onInsert}>등록</button>
             <ul>
                 {list.map((value,index) => (
